ea9a1b47bc4fbf1779fdda79da15eb14cfc4960a
Hector <hectordm65@gmail.com>
2019-04-01 21:43:38 -0700

    fixed images bug

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const path = require('path');
-const generatePassword = require('password-generator');
+const express = require("express");
+const path = require("path");
+const generatePassword = require("password-generator");
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
+const client_id = "2b99e55f6fc04b1c82063242856ab33f";
+const client_secret = "13dc08c45b9749fb80e80d945e0951c3";
+// const redirect_url = ""
 // Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
+app.get("/api/passwords", (req, res) => {
   const count = 5;
 
   // Generate some passwords
   const passwords = Array.from(Array(count).keys()).map(i =>
     generatePassword(12, false)
-  )
+  );
 
   // Return them as json
   res.json(passwords);
@@ -24,8 +27,8 @@ app.get('/api/passwords', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
